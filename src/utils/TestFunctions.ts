b64b1f482219a110d8ad5d56869f6880f28282ff
Canary2000 <its.amelina@proton.me>
2023-09-13 23:14:52 -0300

    Feat: Adding more types
    - Added Number and Json types;

--- src/utils/TestFunctions.ts
+++ src/utils/TestFunctions.ts
@@ -4,23 +4,38 @@ const functions: {
     'string': (input: any) => {
         if (typeof input !== 'string') {
             return false
-        } else {
+        }
+        return true
+
+    },
+    'number': (input: any) => {
+        return typeof input == 'number' && !isNaN(input)
+    },
+    'json': (input: any) => {
+        if (typeof input !== 'string') {
+            return false
+        }
+
+        try {
+            JSON.parse(input)
             return true
+        } catch (error: any) {
+            return false
         }
     },
     'emailaddress': (input: any) => {
         if (typeof input !== 'string') {
             return false
-        } else {
-            return Boolean(String(input).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
         }
+
+        return Boolean(String(input).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
     },
     'datetime': (input: any) => {
         if (typeof input !== 'string' || typeof input !== 'number') {
             return false
-        } else {
-            return (/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/gm.test(input))
         }
+        return (/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/gm.test(input))
+
     },
     'optional': (primitiveTypeTest: Function) => {
         return (input: any) => {
